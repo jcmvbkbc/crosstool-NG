d064f8c831b2bc3b156a84524563f747d76d32ec
Caleb Szalacinski <opensource@skiboy.net>
2016-09-06 22:29:38 -0500

    Fixed problem when retrieving tarball from sourceforge

--- scripts/build/companion_libs/210-expat.sh
+++ scripts/build/companion_libs/210-expat.sh
@@ -9,7 +9,7 @@ do_expat_for_target() { :; }
 if [ "${CT_EXPAT_TARGET}" = "y" -o "${CT_EXPAT}" = "y" ]; then
 
 do_expat_get() {
-    CT_GetFile "expat-${CT_EXPAT_VERSION}" .tar.gz    \
+    CT_GetFile "expat-${CT_EXPAT_VERSION}".tar.gz    \
                http://downloads.sourceforge.net/project/expat/expat/${CT_EXPAT_VERSION}
 }
 
